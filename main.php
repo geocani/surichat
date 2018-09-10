447b7fc91c377a56f6219076bf6acf2be5636df6
Canivet Geoffrey <35733059+geocani@users.noreply.github.com>
2018-09-10 15:50:05 +0200

    maj

--- main.php
+++ main.php
@@ -6,35 +6,62 @@
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel='stylesheet' href='css/style.css'>
+    <link rel='stylesheet' href='css/style-main.css'>
     <script src='https://code.jquery.com/jquery-3.3.1.js' integrity='sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60='crossorigin='anonymous'></script>
 
     <title>TITRE</title>
 </head>
 <body>
 <style>
-html{
-    padding-top: 80px;
-    background-color: rgb(138, 138, 138);
-}
-body{
-    background-color: rgb(138, 138, 138);
-}
-.log_ok{
-    width: 100%;
-}
-.log_50{
-    width: 49%;
-}
-.card{
-    -moz-box-shadow: 1px 1px 5px 0px #656565;
--webkit-box-shadow: 1px 1px 5px 0px #656565;
--o-box-shadow: 1px 1px 5px 0px #656565;
-box-shadow: 1px 1px 5px 0px #656565;
-filter:progid:DXImageTransform.Microsoft.Shadow(color=#656565, Direction=134, Strength=5);
-}
+
 
 </style>
+
+<?php
+// BDD
+$bdd = new PDO("mysql: host=localhost; dbname=surichat; charset=utf8", "root", "");
+
+
+
+
+if (isset($_POST['inscription'])){
+    if (!empty($_POST['inscription_log']) AND !empty($_POST['inscription_email']) AND !empty($_POST['inscription_mdp1']) AND !empty($_POST['inscription_mdp2'])){
+
+        // SANITISATION
+        $login = htmlspecialchars($_POST['inscription_log']);
+        $strlen_login = strlen($login);
+        $email = filter_var($_POST['inscription_email'], FILTER_SANITIZE_EMAIL);
+        $mdp1 = sha1($_POST['inscription_mdp1']);
+        $mdp2 = sha1($_POST['inscription_mdp2']);
+
+        if ($strlen_login <= 50){
+            if ($mdp1 === $mdp2){
+                // if (true === filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                //     $insertmembre = $bdd->prepare("INSERT INTO membres(login, pass, email) VALUES(?, ?, ?)");
+                //     $insertmembre->execute(array($login, $email, $mdp1));
+                //     $succes = "Vous êtes correctement inscrit!";
+                // } else {
+                //     $erreur = "Email non valide";
+                // }
+                    $insertmembre = $bdd->prepare("INSERT INTO membres(login, pass, email) VALUES(?, ?, ?)");
+                    $insertmembre->execute(array($login, $mdp1, $email));
+                    $succses = "Vous êtes correctement inscrit!";
+            }else{
+                $erreur = "Vos mots de passe ne sont pas identique.";
+            }
+        }else{
+            $erreur = "Votre pseudo ne doit pas depasser 50 caractères.";
+        }
+    }else{
+        $erreur = "Vous devez remplire tout les champs.";
+    }
+}
+
+
+
+?>
+
+
 
 <br><br><br>
 <div class="container">
@@ -47,9 +74,11 @@ filter:progid:DXImageTransform.Microsoft.Shadow(color=#656565, Direction=134, St
                 </div>
                 <div class="card-body">
                     <div class="login_suri">
-                        <input type="text" class="log_ok" name="log_ok" id="logOk" placeholder="Login">
-                        <input type="text" class="log_ok" name="pass_ok" id="passOk" placeholder="password">
-                        <input type="submit" class="log_ok" name="login_ok" id="loginOk" value="Login">
+                        <form action="" method="POST">
+                            <input type="text" class="log_ok" name="log_ok" id="logOk" placeholder="Login">
+                            <input type="text" class="log_ok" name="pass_ok" id="passOk" placeholder="password">
+                            <input type="submit" class="log_ok" name="login_ok" id="loginOk" value="Login">
+                        </form>
                     </div>
                 </div>
             </div>
@@ -63,16 +92,20 @@ filter:progid:DXImageTransform.Microsoft.Shadow(color=#656565, Direction=134, St
                 </div>
                 <div class="card-body">
                     <div class="login_suri">
-                        <input type="text" class="log_50" name="log_ok" id="logOk" placeholder="Login">
-                        <input type="text" class="log_50" name="pass_ok" id="passOk" placeholder="Email">
-                        <input type="text" class="log_50" name="pass_ok" id="passOk" placeholder="password"> 
-                        <input type="text" class="log_50" name="pass_ok" id="passOk" placeholder="Confirm password"> 
-                        <input type="submit" class="log_ok" name="login_ok" id="loginOk" value="Login">
+                        <form action="" method="POST">
+                            <input type="text" class="log_50" name="inscription_log" id="logOk" placeholder="Login">
+                            <input type="text" class="log_50" name="inscription_email" id="passOk" placeholder="Email">
+                            <input type="text" class="log_50" name="inscription_mdp1" id="passOk" placeholder="password"> 
+                            <input type="text" class="log_50" name="inscription_mdp2" id="passOk" placeholder="Confirm password"> 
+                            <input type="submit" class="log_ok" name="inscription" id="inscription" value="Inscription">
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
         <!-- End Card 2 -->
+        <?php if (isset($erreur)){ echo "<p class='erreur'>" . $erreur . "</p>"; }; ?>
+        <?php if (isset($succses)){ echo "<p class='succses'>" . $succses . "</p>"; }; ?>
     </div>
 </div>
 
