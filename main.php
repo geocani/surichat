d605fc7c591ac473216aac5485b9321e6ffeb0ad
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-09-10 09:07:16 +0100

    maj

---
+++ main.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel='stylesheet' href='css/style.css'>
+    <script src='https://code.jquery.com/jquery-3.3.1.js' integrity='sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60='crossorigin='anonymous'></script>
+
+    <title>TITRE</title>
+</head>
+<body>
+<style>
+html{
+    padding-top: 80px;
+    background-color: rgb(138, 138, 138);
+}
+body{
+    background-color: rgb(138, 138, 138);
+}
+.log_ok{
+    width: 100%;
+}
+.log_50{
+    width: 49%;
+}
+.card{
+    -moz-box-shadow: 1px 1px 5px 0px #656565;
+-webkit-box-shadow: 1px 1px 5px 0px #656565;
+-o-box-shadow: 1px 1px 5px 0px #656565;
+box-shadow: 1px 1px 5px 0px #656565;
+filter:progid:DXImageTransform.Microsoft.Shadow(color=#656565, Direction=134, Strength=5);
+}
+
+</style>
+
+<br><br><br>
+<div class="container">
+    <div class="row"> 
+        <!-- Card 1 -->
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Connexion</h2>
+                </div>
+                <div class="card-body">
+                    <div class="login_suri">
+                        <input type="text" class="log_ok" name="log_ok" id="logOk" placeholder="Login">
+                        <input type="text" class="log_ok" name="pass_ok" id="passOk" placeholder="password">
+                        <input type="submit" class="log_ok" name="login_ok" id="loginOk" value="Login">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Card 1 -->
+        <!-- Card 2 -->
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Inscription</h2>
+                </div>
+                <div class="card-body">
+                    <div class="login_suri">
+                        <input type="text" class="log_50" name="log_ok" id="logOk" placeholder="Login">
+                        <input type="text" class="log_50" name="pass_ok" id="passOk" placeholder="Email">
+                        <input type="text" class="log_50" name="pass_ok" id="passOk" placeholder="password"> 
+                        <input type="text" class="log_50" name="pass_ok" id="passOk" placeholder="Confirm password"> 
+                        <input type="submit" class="log_ok" name="login_ok" id="loginOk" value="Login">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Card 2 -->
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src='js/script.js'></script>
+<script>
+
+</script>
+</body>
+</html>
